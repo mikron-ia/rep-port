44f167213d3bcb22469158367d1696ec252e258d
Mikron <mikron-ia@users.noreply.github.com>
2019-08-14 14:07:25 +0200

    :boom:

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="header">
+      <h1>RepPort</h1>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div id="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./../node_modules/bootstrap/scss/bootstrap";
+@import "./../node_modules/bootstrap-vue/src/index.scss";
+@import "assets/app";
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <p>
+      This is an application for checking person's reputation networks. To
+      retrieve the reputation data, choose a person from the list. If more
+      detailed data is required, choose a desired network.
+    </p>
+    <p>
+      Note: some networks are more private than others; if you do not see
+      someone's history or even their score, you might have insufficient
+      standing in the network yourself.
+    </p>
+  </div>
+</template>
